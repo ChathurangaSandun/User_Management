3a268ba35ae9313df3564789803c140bf6f717db
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2017-05-20 21:20:27 +0530

    remove space

--- UserManagmentModule.Api/Controller/ScreenComponentController.cs
+++ UserManagmentModule.Api/Controller/ScreenComponentController.cs
@@ -12,8 +12,7 @@ namespace UserManagmentModule.Api.Controller
     [RoutePrefix("api/ScreenComponent")]
     public class ScreenComponentController : ApiController
     {
-
-
+         
         [Route("GetActiveComponents/{screenName}")]
         [HttpGet]
         public List<string> GetActiveComponentsFromScreen(string screenName)
