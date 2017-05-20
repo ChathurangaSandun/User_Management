155577b67aad71fc63bad1d7034de13f27482747
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2017-05-20 18:43:33 +0530

    Add project files.

---
+++ UserManagmentModule.Api/Controller/CustomController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserManagmentModule.Api.Manages;
+using UserManagmentModule.DatabaseConnection.Workers;
+
+namespace UserManagmentModule.Api.Controller
+{
+    public class CustomController : ApiController
+    {
+
+        
+
+        [Route("api/custom/get")]
+        [HttpGet]
+        public IDictionary<int ,string> Get()
+        {          
+            return CustomManager.AllScreens();
+        }
+    }
+}
---
+++ UserManagmentModule.DatabaseConnection/Factory/DatabaseFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagmentModule.DatabaseConnection.Common;
+using UserManagmentModule.DatabaseConnection.Handler;
+
+namespace UserManagmentModule.DatabaseConnection.Factory
+{
+    public sealed class DatabaseFactory
+    {
+        public static DatabaseFactorySectionHandler sectionHandler = (DatabaseFactorySectionHandler)ConfigurationManager.GetSection("DatabaseFactoryConfiguration");
+        private DatabaseFactory() { }
+        public static Database CreateDatabase()
+        {
+            // Verify a DatabaseFactoryConfiguration line exists in the web.config.
+            if (sectionHandler.Name.Length == 0)
+            {
+                throw new Exception("Database name not defined in DatabaseFactoryConfiguration section of web.config.");
+            }
+            try
+            {
+                // Find the class
+                Type database = Type.GetType(sectionHandler.Name);
+                // Get it's constructor
+                ConstructorInfo constructor = database.GetConstructor(new Type[] { });
+                // Invoke it's constructor, which returns an instance.
+                Database createdObject = (Database)constructor.Invoke(null);
+                // Initialize the connection string property for the database.
+                createdObject.connectionString = sectionHandler.ConnectionString;
+                // Pass back the instance as a Database
+                return createdObject;
+            }
+            catch (Exception excep)
+            {
+                throw new Exception("Error instantiating database " + sectionHandler.Name + ". " + excep.Message);
+            }
+        }
+    }
+}
